3dcdbc996b7351f76169eb52d4d69fce3bbe2cba
bhavesh27 <bhaveshkumar19856@gmail.com>
2020-05-04 10:57:30 +0530

    Removed Config

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-module.exports = {
-    'JWT_SECRET': 'mysuperjwtsecret',
-    'PORT': 3000,
-    'MONGODB_URI': 'mongodb://root:roottoor123@dbh29.mlab.com:27297/identity',
-    'TWILIO_ACCOUNT_SID': 'AC5d7d7ecb77b41ba9f4f12a3130fda30b',
-    'TWILIO_AUTHTOKEN': '7b4f7c547e988296afc30f02f4c36ca6',
-    'TWILIO_TEST_NUMBER': '(202) 519-7726'
-};
